31440981d63b730c8ec90f782311223689558798
Mariusz Nowak <mariusz@medikoo.com>
2019-09-13 13:50:55 +0200

    feat: Skip with Notice utils

---
+++ docs/skip-on-disabled-symlinks-in-windows.md
@@ -0,0 +1,18 @@
+# skip-on-disabled-symlinks-in-windows
+
+Preconfigured [`skip-with-notice`](skip-with-notice.md) usage to skip on Windows symlink errors.
+
+Usage:
+
+```javascript
+const skipOnDisabledSymlinksInWindows = require('@serverless/test/skip-on-disabled-symlinks-in-windows');
+
+describe('Some suite', () => {
+  it('Some test that involves symlinks creation', function() {
+    ensureSymlink(realFilePath, symlinkPath).catch(error => {
+      skipOnWindowsDisabledSymlinks(error, this);
+      throw error;
+    });
+  });
+});
+```
---
+++ skip-with-notice.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const chalk = require('chalk');
+const { deferredRunner } = require('./setup/mocha-reporter');
+
+const skippedWithNotice = [];
+
+module.exports = (context, reason, afterCallback) => {
+  if (!context || typeof context.skip !== 'function') {
+    throw new TypeError('Passed context is not a valid mocha suite');
+  }
+  if (process.env.CI) return; // Do not tolerate skips in CI environment
+
+  skippedWithNotice.push({ context, reason });
+  process.stdout.write(chalk.yellow(`\n Skipped due to: ${chalk.red(reason)}\n\n`));
+
+  if (afterCallback) {
+    try {
+      // Ensure teardown is called
+      // (Mocha fails to do it -> https://github.com/mochajs/mocha/issues/3740)
+      afterCallback();
+    } catch (error) {
+      process.stdout.write(chalk.error(`after callback crashed with: ${error.stack}\n`));
+    }
+  }
+  context.skip();
+};
+
+deferredRunner.then(runner =>
+  runner.on('end', () => {
+    // Output eventual skip notices
+    if (!skippedWithNotice.length) return;
+
+    const resolveTestName = test => {
+      const names = [test.title];
+      let parent = test.parent;
+      while (parent) {
+        if (parent.title) names.push(parent.title);
+        parent = parent.parent;
+      }
+      return `${chalk.cyan(names.reverse().join(': '))} (in: ${chalk.grey(
+        test.file.slice(process.cwd().length + 1)
+      )})`;
+    };
+
+    process.stdout.write(
+      ' Notice: Some tests were skipped due to following environment issues:' +
+        `\n\n - ${skippedWithNotice
+          .map(meta => `${resolveTestName(meta.context.test)}\n\n   ${chalk.red(meta.reason)}\n`)
+          .join('\n - ')}\n\n`
+    );
+  })
+);
