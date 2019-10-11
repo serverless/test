ca1deac0fa5ec9e0afecca26b5614b38da656480
Mariusz Nowak <mariusz@medikoo.com>
2019-10-11 14:05:20 +0200

    feat: `provision-tmp-dir` util
    Creates temp directory for single need and returns path to it

---
+++ provision-tmp-dir.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const path = require('path');
+const crypto = require('crypto');
+const { mkdir } = require('fs-extra');
+const processTmpDir = require('./process-tmp-dir');
+
+module.exports = () =>
+  new Promise(resolve => {
+    const tmpDirName = path.join(processTmpDir, crypto.randomBytes(3).toString('hex'));
+    resolve(
+      mkdir(tmpDirName).then(
+        () => tmpDirName,
+        error => {
+          if (error.code !== 'EEXIST') throw error;
+          return module.exports(); // Name taken (rare edge case), retry
+        }
+      )
+    );
+  });
