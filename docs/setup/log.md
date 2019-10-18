0e082392395ad1509210d21f6defb8d3441fda8e
Mariusz Nowak <mariusz@medikoo.com>
2019-10-18 15:17:52 +0200

    feat: In logs setup by default flush observed logs on test failure

--- docs/setup/log.md
+++ docs/setup/log.md
@@ -1,3 +1,6 @@
 # log
 
-Configure log writter to expose logs written via [log](https://github.com/medikoo/log#log) utility
+Configure log writter to expose logs written via [log](https://github.com/medikoo/log#log) utility.
+
+By default (when no logging configuration is found in enviroment) all emitted logs (down to _debug_ level) are observed
+and output in case of test failure.
--- setup/log.js
+++ setup/log.js
@@ -1,3 +1,36 @@
 'use strict';
 
-require('log-node')();
+const initializeLogWriter = require('log-node');
+
+const logWriter = initializeLogWriter();
+
+if (process.env.LOG_LEVEL || process.env.LOG_DEBUG || process.env.DEBUG) return;
+
+// Flush all gathered logs (down to DEBUG level) on test failure
+
+const logEmitter = require('log/lib/emitter');
+const { deferredRunner } = require('./mocha-reporter');
+
+const logsBuffer = [];
+const flushLogs = () => {
+  logsBuffer.forEach(event => {
+    if (!event.message) logWriter.resolveMessage(event);
+    logWriter.writeMessage(event);
+  });
+  logsBuffer.length = 0; // Empty array
+};
+
+logEmitter.on('log', event => {
+  logsBuffer.push(event);
+  if (!event.message) logWriter.resolveMessageTokens(event);
+});
+deferredRunner.then(runner => {
+  runner.on('suite end', suite => {
+    if (!suite.parent || !suite.parent.root) return;
+
+    logsBuffer.length = 0; // Empty array
+  });
+  runner.on('fail', flushLogs);
+});
+
+module.exports.flushLogs = flushLogs;
