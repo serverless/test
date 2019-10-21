df7d61ec4b0919f637696d1c5416f49343fe9e6f
Mariusz Nowak <mariusz@medikoo.com>
2019-10-21 15:44:22 +0200

    refactor: Rely on process-utils/create-env

--- resolve-env.js
+++ resolve-env.js
@@ -1,9 +1,9 @@
 'use strict';
 
-module.exports = () => {
-  const env = { SLS_TRACKING_DISABLED: '1' };
-  for (const varName of ['APPDATA', 'HOME', 'PATH', 'TMPDIR', 'USERPROFILE']) {
-    if (process.env[varName]) env[varName] = process.env[varName];
-  }
-  return env;
-};
+const createEnv = require('process-utils/create-env');
+
+module.exports = () =>
+  createEnv({
+    whitelist: ['APPDATA', 'HOME', 'PATH', 'TMPDIR', 'USERPROFILE'],
+    variables: { SLS_TRACKING_DISABLED: '1' },
+  });
