2afe71fadbbd003a8d286900d76976ae691c769f
Mariusz Nowak <mariusz@medikoo.com>
2019-11-22 16:16:46 +0100

    feat: 'configureAwsRequestStub' util

---
+++ configure-aws-request-stub.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const ensureObject = require('type/object/ensure');
+const ensurePlainObject = require('type/plain-object/ensure');
+const ensureFunction = require('type/plain-function/ensure');
+const sinon = require('sinon');
+
+module.exports = (provider, config) => {
+  ensureObject(provider);
+  ensureFunction(provider.request);
+  ensurePlainObject(config);
+
+  if (provider.request.restore) provider.request.restore();
+
+  return sinon.stub(provider, 'request').callsFake(
+    (service, method) =>
+      new Promise((resolve, reject) => {
+        if (!config[service] || !config[service][method]) {
+          reject(new Error(`Missing AWS request stub configuration for ${service}.${method}`));
+          return;
+        }
+        resolve(config[service][method]);
+      })
+  );
+};
