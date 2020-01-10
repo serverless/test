1c9db3778ee818e40439fa248a95f57312a80dfe
Mariusz Nowak <mariusz@medikoo.com>
2020-01-10 17:41:39 +0100

    refactor: Whitelist env via options in resolveAwsEnv

--- resolve-aws-env.js
+++ resolve-aws-env.js
@@ -3,12 +3,9 @@
 const resolveEnv = require('./resolve-env');
 
 module.exports = () => {
-  const env = resolveEnv();
+  const env = resolveEnv({ whitelist: ['SERVERLESS_ACCESS_KEY'] });
   for (const envVarName of Object.keys(process.env)) {
     if (envVarName.startsWith('AWS_')) env[envVarName] = process.env[envVarName];
-  }
-  if (process.env.SERVERLESS_ACCESS_KEY) {
-    env.SERVERLESS_ACCESS_KEY = process.env.SERVERLESS_ACCESS_KEY;
   }
   return env;
 };
