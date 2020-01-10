7b6d31203f9162ecdfc1c5dbd930350846852592
Mariusz Nowak <mariusz@medikoo.com>
2020-01-10 11:01:08 +0100

    fix: Do not hard crash on async leaks detection
    It'll help to diagnoze source of async leak

--- setup/async-leaks-detector/index.js
+++ setup/async-leaks-detector/index.js
@@ -2,13 +2,16 @@
 
 if (process.version.match(/\d+/)[0] < 8) return; // Async leaks detector is not reliable in Node.js v6
 
+const chalk = require('chalk');
+
 require('../mocha-reporter').deferredRunner.then(runner =>
   runner.on('end', () => {
     // Async leaks detection
     setTimeout(() => {
       // If tests end with any orphaned async call then this callback will be invoked
       // It's a signal there's some promise chain (or in general async flow) misconfiguration
-      throw new Error('Test ended with unfinished async jobs');
+      process.stdout.write(chalk.red('Error: Test ended with unfinished async jobs\n'));
+      process.on('exit', () => (process.exitCode = 1));
       // Timeout '2' to ensure no false positives, with '1' there are observable rare scenarios
       // of possibly a garbage collector delaying process exit being picked up
     }, 5).unref();
