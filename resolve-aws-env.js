69659bf3fb0b4a9271916170047df13b32567eb2
Mariusz Nowak <mariusz@medikoo.com>
2020-01-10 17:42:52 +0100

    feat: Recognize dashboard test setup env vars

--- resolve-aws-env.js
+++ resolve-aws-env.js
@@ -3,7 +3,13 @@
 const resolveEnv = require('./resolve-env');
 
 module.exports = () => {
-  const env = resolveEnv({ whitelist: ['SERVERLESS_ACCESS_KEY'] });
+  const env = resolveEnv({
+    whitelist: [
+      'SERVERLESS_ACCESS_KEY',
+      'SERVERLESS_PLATFORM_TEST_APP',
+      'SERVERLESS_PLATFORM_TEST_ORG',
+    ],
+  });
   for (const envVarName of Object.keys(process.env)) {
     if (envVarName.startsWith('AWS_')) env[envVarName] = process.env[envVarName];
   }
