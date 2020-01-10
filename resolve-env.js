f5d6a1ed3ac5c28b85f9fd5d1991a4ac729b5e38
Mariusz Nowak <mariusz@medikoo.com>
2020-01-10 17:36:11 +0100

    refactor: Support env extensions in options of createEnv

--- resolve-env.js
+++ resolve-env.js
@@ -2,8 +2,17 @@
 
 const createEnv = require('process-utils/create-env');
 
-module.exports = () =>
-  createEnv({
-    whitelist: ['APPDATA', 'HOME', 'PATH', 'SERVERLESS_BINARY_PATH', 'TMPDIR', 'USERPROFILE'],
-    variables: { SLS_TRACKING_DISABLED: '1' },
+module.exports = (options = {}) => {
+  if (!options) options = {};
+  return createEnv({
+    whitelist: [
+      'APPDATA',
+      'HOME',
+      'PATH',
+      'SERVERLESS_BINARY_PATH',
+      'TMPDIR',
+      'USERPROFILE',
+    ].concat(options.whitelist || []),
+    variables: Object.assign({ SLS_TRACKING_DISABLED: '1' }, options.variables || {}),
   });
+};
