b515663d6c971143461c08457d1e056bedec4dd6
Mariusz Nowak <mariusz@medikoo.com>
2020-02-12 17:02:58 +1300

    refactor: Ensure to whitelist all SERVERLESS_PLATFORM_TEST_* env vars

--- resolve-aws-env.js
+++ resolve-aws-env.js
@@ -4,14 +4,12 @@ const resolveEnv = require('./resolve-env');
 
 module.exports = () => {
   const env = resolveEnv({
-    whitelist: [
-      'SERVERLESS_ACCESS_KEY',
-      'SERVERLESS_PLATFORM_TEST_APP',
-      'SERVERLESS_PLATFORM_TEST_ORG',
-    ],
+    whitelist: ['SERVERLESS_ACCESS_KEY'],
   });
   for (const envVarName of Object.keys(process.env)) {
-    if (envVarName.startsWith('AWS_')) env[envVarName] = process.env[envVarName];
+    if (envVarName.startsWith('AWS_') || envVarName.startsWith('SERVERLESS_PLATFORM_TEST_')) {
+      env[envVarName] = process.env[envVarName];
+    }
   }
   return env;
 };
