90d71a1cb3cd7d4a72b30214a5626a718578fef3
Mariusz Nowak <medyk@medikoo.com>
2020-06-03 15:31:51 +0200

    feat: Handle internal module rename in `serverless`

--- disable-serverless-stats-requests.js
+++ disable-serverless-stats-requests.js
@@ -3,13 +3,25 @@
 'use strict';
 
 const path = require('path');
+const isModuleNotFoundError = require('ncjsm/is-module-not-found-error');
+
+const resolveAreAnalyticsDisabled = (serverlessPath) => {
+  let modulePath = path.join(serverlessPath, 'lib/utils/analytics/areDisabled');
+  try {
+    return require.resolve(modulePath);
+  } catch (error) {
+    if (!isModuleNotFoundError(error, modulePath)) throw error;
+  }
+  modulePath = path.join(serverlessPath, 'lib/utils/isTrackingDisabled');
+  return require.resolve(modulePath);
+};
 
 module.exports = (serverlessPath) => {
-  const modulePath = path.join(serverlessPath, 'lib/utils/isTrackingDisabled');
+  const modulePath = resolveAreAnalyticsDisabled(serverlessPath);
   // eslint-disable-next-line import/no-unresolved
-  const isTrackingDisabled = require(modulePath);
+  const areAnalyticsDisabled = require(modulePath);
 
-  if (!isTrackingDisabled) {
+  if (!areAnalyticsDisabled) {
     // Ensure no tracking during tests run
     require.cache[require.resolve(modulePath)].exports = true;
   }
