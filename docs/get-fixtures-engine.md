bf856abcc228f8dd27e171859d739192e6004848
Mariusz Nowak <medyk@medikoo.com>
2020-09-08 16:47:53 +0200

    feat(Fixtures Engine): Reconfigure and ntroduce single fixtures.setup
    BREAKING CHANGE:
    `fixtures.map`, `fixtures.extend` were removed and `fixtures.setup` was redesigned to address all fixture setup cases.
    Now each fixture is copied to temporary folder, which is retured by `fixtures.setup`

--- docs/get-fixtures-engine.md
+++ docs/get-fixtures-engine.md
@@ -1,6 +1,6 @@
 # get-fixtures-engine
 
-Initializes _fixtures_ engine for a folder where Servelress service fixtures are placed (each in different top level folder)
+Initializes _fixtures_ engine for a folder where Serverless service fixtures are placed (each in different top level folder)
 
 Fixtures engine provides out of a box means to extend existing fixtures (enrich/tweak configurations of existing fixutres for testing purpused) and to cleanup eventually created artifacts after testing is made
 
@@ -12,52 +12,31 @@ Fixture engine should be initialized by passing a path to directory that contain
 const fixtures = require('@serverless/test/get-fixtures-engine')(fixturesPath);
 ```
 
-### 1. Retrieve fixture as is
+### Preparing a fixture
 
-Simply to resolve full path to existing fixture rely on `fixtures.map[fixtureDirectoryName]`.
-It'll respond with full path to fixture. However it'll additionally:
+Fixture is a simple, minimal service, configured to help test Framework functionalities.
 
-- Crash immediately if fixture is not found
-- Registers fixtures for cleanup operation after testing is done
+Fixture may have `_setup.js` script, which should cover an additional setup (if it's needed), before fixture is used.
 
-```javascript
-const pathToHttpFixture = fixturesEngine.http;
-```
+### Resolving a fixture
 
-### 2. Retrieve fixture and extend its service configuration
+Through a `fixtures.setup(fixtureName, options)` we retrieve a temporary path to fully setup copied fixture.
 
-In this case given fixture is copied to a temp dir where modified `serverless.yml` configuration is placed
+Each `setup` call, prepares different fixture copy.
 
-```javascript
-// Generates extended fixture (in temporary path) and returns its path
-const pathToExtendedHttpFixture = await fixturesEngine.extend('http', {
-  provider: { logs: { restApi: true } },
-});
-```
+Fixture is prepared in temporary directory which is removed after tests are done.
 
-### 3. Retrieve a fixture with implied _setup_ setup
+Each setup fixture is provided with unique service name, which allows to use them for integration tests without risking service name collisions.
 
-In some scenarios fixture may require a setup step.
+#### Supported options
 
-In given case fixture should be created with a `_setup.js` module in its folder.
+##### `configExt`
 
-Module right in its body should start a _setup_ process and export a promise that resolves once its done.
-
-Additionally module may resolve with a _cleanup instructions_ (configuration object).
-
-Currently all that's supported in there is `pathsToRemove` property, which indicates additional paths for cleanup operation
+Config extesions which should be deeply merged onto existing config. Through that existing configuration can be additionally detailed for tested case. We may add new functions or configure extra events.
 
 ```javascript
 // Generates extended fixture (in temporary path) and returns its path
-const pathToExtendedHttpFixture = await fixturesEngine.extend('http', {
-  provider: { logs: { restApi: true } },
+const pathToExtendedHttpFixture = await fixturesEngine.setup('http', {
+  configExt: { provider: { logs: { restApi: true } } },
 });
-```
-
-### Cleanup after testing is done
-
-For proper cleanup `fixtures.cleanup` needs to be registered in `after`:
-
-```javascript
-after(fixtures.cleanup);
 ```
--- get-fixtures-engine.js
+++ get-fixtures-engine.js
@@ -3,10 +3,12 @@
 const path = require('path');
 const ensureString = require('type/string/ensure');
 const ensurePlainObject = require('type/plain-object/ensure');
+const ensurePlainFunction = require('type/plain-function/ensure');
 const BbPromise = require('bluebird');
 const fse = require('fs-extra');
 const memoizee = require('memoizee');
 const { merge } = require('lodash');
+const log = require('log').get('serverless:test');
 const { load: loadYaml, dump: saveYaml } = require('js-yaml');
 const provisionTmpDir = require('./provision-tmp-dir');
 
@@ -21,106 +23,96 @@ const isFixtureConfigured = memoizee((fixturePath) => {
   return Boolean(stats.isDirectory());
 });
 
-const retrievedFixturesPaths = new Set();
-const currentlySetupFixtures = new Set();
-const retrievedFixturesCleanupInstructions = new Map();
-
 const setupFixture = memoizee(
   (fixturePath) =>
-    BbPromise.resolve(require(path.join(fixturePath, '_setup'))()).then((cleanupInstructions) => {
-      currentlySetupFixtures.add(fixturePath);
-      if (cleanupInstructions) {
-        retrievedFixturesCleanupInstructions.set(fixturePath, cleanupInstructions);
-      }
-    }),
+    fse
+      .lstat(path.resolve(fixturePath, '_setup.js'))
+      .then(
+        (stats) => {
+          if (!stats.isFile()) return false;
+          return true;
+        },
+        (error) => {
+          if (error.code === 'ENOENT') return false;
+          throw error;
+        }
+      )
+      .then((isSetupScript) => {
+        if (!isSetupScript) return fixturePath;
+        return provisionTmpDir().then((setupFixturePath) => {
+          const setupScriptPath = path.resolve(setupFixturePath, '_setup.js');
+          return fse
+            .copy(fixturePath, setupFixturePath)
+            .then(() => ensurePlainFunction(require(setupScriptPath))(fixturePath))
+            .then(() => fse.unlink(setupScriptPath))
+            .then(() => setupFixturePath);
+        });
+      }),
   { promise: true }
 );
 
+const nameTimeBase = new Date(2020, 8, 7).getTime();
+
 module.exports = memoizee((fixturesPath) => {
   const fixturesEngine = {
-    map: new Proxy(
-      {},
-      {
-        get: (obj, fixtureName) => {
-          const fixturePath = path.join(fixturesPath, fixtureName);
-          if (!isFixtureConfigured(fixturePath)) {
-            throw new Error(`No fixture configured at ${fixtureName}`);
-          }
-          retrievedFixturesPaths.add(fixturePath);
-          return fixturePath;
-        },
-      }
-    ),
-    setup: (fixtureName) =>
-      BbPromise.try(() => {
-        const fixturePath = path.join(fixturesPath, ensureString(fixtureName));
-        if (!isFixtureConfigured(fixturePath)) {
-          throw new Error(`No fixture configured at ${fixtureName}`);
-        }
-
-        return setupFixture(fixturePath).then(() => {
-          retrievedFixturesPaths.add(fixturePath);
-          return fixturePath;
-        });
-      }),
-    extend: (fixtureName, extConfig) =>
+    setup: (fixtureName, options = {}) =>
       BbPromise.try(() => {
         const baseFixturePath = path.join(fixturesPath, ensureString(fixtureName));
         if (!isFixtureConfigured(baseFixturePath)) {
           throw new Error(`No fixture configured at ${fixtureName}`);
         }
+        if (!options) options = {};
 
-        ensurePlainObject(extConfig);
-        return Promise.all([
-          provisionTmpDir(),
-          fse.lstat(path.resolve(baseFixturePath, '_setup.js')).then(
-            (stats) => {
-              if (!stats.isFile()) return null;
-              return fixturesEngine.setup(fixtureName);
-            },
-            (error) => {
-              if (error.code !== 'ENOENT') throw error;
-            }
-          ),
-        ]).then(([fixturePath]) => {
-          return Promise.all([
-            fse.readFile(path.join(baseFixturePath, 'serverless.yml')),
-            fse.copy(baseFixturePath, fixturePath),
-          ])
-            .then(([yamlConfig]) =>
-              fse.writeFile(
-                path.join(fixturePath, 'serverless.yml'),
-                saveYaml(merge(loadYaml(yamlConfig), extConfig))
-              )
-            )
-            .then(() => fixturePath);
-        });
+        return Promise.all([provisionTmpDir(), setupFixture(baseFixturePath)]).then(
+          ([fixturePath, setupFixturePath]) => {
+            let configObject;
+            return Promise.all([
+              fse.readFile(path.join(setupFixturePath, 'serverless.yml')).catch((error) => {
+                if (error.code === 'ENOENT') return null;
+                throw error;
+              }),
+              fse.copy(setupFixturePath, fixturePath),
+            ])
+              .then(([configContent]) => {
+                if (!configContent) return null;
+                configObject = (() => {
+                  try {
+                    return loadYaml(configContent);
+                  } catch (error) {
+                    return null;
+                  }
+                })();
+                if (!configObject) return null;
+                if (!configObject.service && !options.configExt) return null;
+
+                configObject.service = `test-${fixtureName}-${(Date.now() - nameTimeBase).toString(
+                  32
+                )}`;
+                if (options.configExt) configObject = merge(configObject, options.configExt);
+
+                return fse.writeFile(
+                  path.join(fixturePath, 'serverless.yml'),
+                  saveYaml(configObject)
+                );
+              })
+              .then(() => {
+                log.info('setup %s fixture at %s', fixtureName, fixturePath);
+                return {
+                  servicePath: fixturePath,
+                  serviceConfig: configObject,
+                  updateConfig: (configExt) => {
+                    ensurePlainObject(configExt);
+                    merge(configObject, configExt);
+                    return fse.writeFile(
+                      path.join(fixturePath, 'serverless.yml'),
+                      saveYaml(configObject)
+                    );
+                  },
+                };
+              });
+          }
+        );
       }),
-    cleanup: (options = {}) =>
-      Promise.all(
-        Array.from(retrievedFixturesPaths, (fixturePath) => {
-          const pathsToRemove = [path.join(fixturePath, '.serverless')];
-          if (options.extraPaths) {
-            pathsToRemove.push(
-              ...options.extraPaths.map((target) => path.resolve(fixturePath, target))
-            );
-          }
-          const cleanupInstructions = retrievedFixturesCleanupInstructions.get(fixturePath);
-          if (cleanupInstructions && cleanupInstructions.pathsToRemove) {
-            pathsToRemove.push(
-              ...cleanupInstructions.pathsToRemove.map((target) =>
-                path.resolve(fixturePath, target)
-              )
-            );
-          }
-          return Promise.all(pathsToRemove.map((pathToRemove) => fse.remove(pathToRemove))).then(
-            () => {
-              retrievedFixturesPaths.delete(fixturePath);
-              setupFixture.clear(fixturePath);
-            }
-          );
-        })
-      ),
   };
   return fixturesEngine;
 });
