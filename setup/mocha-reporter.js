34f0052bdd8f3380303254913778e4c7a3252fe5
Mariusz Nowak <medyk@medikoo.com>
2020-11-24 11:46:18 +0100

    fix(Mocha Fixes): Fix tests propagation patch

--- setup/mocha-reporter.js
+++ setup/mocha-reporter.js
@@ -11,16 +11,21 @@ process.on('unhandledRejection', (err) => {
 const Spec = require('mocha/lib/reporters/spec');
 const Runner = require('mocha/lib/runner');
 
-// Ensure faster tests propagation
-// It's to expose errors otherwise hidden by race conditions
-// Reported to Mocha with: https://github.com/mochajs/mocha/issues/3920
-Runner.immediately = process.nextTick;
-
 let resolveRunner;
 class ServerlessSpec extends Spec {
   constructor(runner) {
     super(runner);
     resolveRunner(runner);
+    process.nextTick(() => {
+      // Ensure faster tests propagation
+      // It's to expose errors otherwise hidden by race conditions
+      // Reported to Mocha with: https://github.com/mochajs/mocha/issues/3920
+      //
+      // Override needs to be done in next tick to ensure other Mocha extensions attach to resolved
+      // runner before any tests are being run
+      // (Race condition was observed, in which `runner` was resolved afterwards)
+      Runner.immediately = process.nextTick;
+    });
 
     process.on('uncaughtException', (err) => {
       if (
