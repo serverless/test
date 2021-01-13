1b70ebf6b50137fe25d3fb1f2dc65cdc4e8ab184
Mariusz Nowak <medyk@medikoo.com>
2021-01-13 13:58:03 +0100

    feat(Mocha): Patch circular references parallel invocation bug

--- setup/patch.js
+++ setup/patch.js
@@ -10,11 +10,39 @@ const EventEmitter = require('events');
 
 const runnerEmitter = new EventEmitter();
 
+const isObject = require('type/object/is');
 const Mocha = require('mocha/lib/mocha');
+const { serialize } = require('mocha/lib/nodejs/serializer');
+
+const removeCyclicReferences = (object, parentObjects = new Set()) => {
+  const entries = Array.isArray(object) ? object.entries() : Object.entries(object);
+  parentObjects = new Set([...parentObjects, object]);
+  for (const [key, value] of entries) {
+    if (!isObject(value)) continue;
+    if (parentObjects.has(value)) delete object[key];
+    else removeCyclicReferences(value, parentObjects);
+  }
+};
 
 const mochaRun = Mocha.prototype.run;
 Mocha.prototype.run = function (fn, ...args) {
-  const runner = mochaRun.call(this, fn, ...args);
+  const runner = mochaRun.call(
+    this,
+    (result) => {
+      // Workaround https://github.com/mochajs/mocha/issues/4552
+      const serialized = serialize(result);
+      const stringifiedResult = (() => {
+        try {
+          return JSON.stringify(serialized);
+        } catch (error) {
+          removeCyclicReferences(serialized);
+          return JSON.stringify(serialized);
+        }
+      })();
+      return fn.call(this, JSON.parse(stringifiedResult));
+    },
+    ...args
+  );
   if (runner.constructor.name === 'Runner') runnerEmitter.emit('runner', runner);
   runner.constructor.immediately = process.nextTick;
   return runner;
