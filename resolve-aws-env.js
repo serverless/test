87973f7b12bd2c8aa0ab0efa5d6e2fbc92f18cdc
Mariusz Nowak <medyk@medikoo.com>
2021-12-28 15:19:55 +0100

    feat(Environment): Allow env vars starting with `SERVERLESS_PLATFORM_`

--- resolve-aws-env.js
+++ resolve-aws-env.js
@@ -7,7 +7,7 @@ module.exports = () => {
     whitelist: ['SERVERLESS_ACCESS_KEY', 'SLS_AWS_REQUEST_MAX_RETRIES'],
   });
   for (const envVarName of Object.keys(process.env)) {
-    if (envVarName.startsWith('AWS_') || envVarName.startsWith('SERVERLESS_PLATFORM_TEST_')) {
+    if (envVarName.startsWith('AWS_') || envVarName.startsWith('SERVERLESS_PLATFORM_')) {
       env[envVarName] = process.env[envVarName];
     }
   }
