d1bb82456549238dfb0b0b518e38e853a154300d
Mariusz Nowak <medyk@medikoo.com>
2022-01-25 14:09:47 +0100

    refactor: Update to rename changes on `serverless` side

--- disable-serverless-stats-requests.js
+++ disable-serverless-stats-requests.js
@@ -4,24 +4,8 @@
 
 const path = require('path');
 
-const resolveAreAnalyticsDisabled = (serverlessPath) => {
-  for (const relativeModulePath of [
-    'lib/utils/telemetry/areDisabled',
-    'lib/utils/analytics/areDisabled',
-  ]) {
-    try {
-      return require.resolve(path.join(serverlessPath, relativeModulePath));
-    } catch {
-      // Pass
-    }
-  }
-
-  const modulePath = path.join(serverlessPath, 'lib/utils/isTrackingDisabled');
-  return require.resolve(modulePath);
-};
-
 module.exports = (serverlessPath) => {
-  const modulePath = resolveAreAnalyticsDisabled(serverlessPath);
+  const modulePath = path.join(serverlessPath, 'lib/utils/telemetry/are-disabled');
   // Ensure no tracking during tests run
   require.cache[require.resolve(modulePath)] = { exports: true };
 };
